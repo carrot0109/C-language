0755334e1651e69edf69e7441926c9b3410d05b8
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-06-29 16:31:59 +0900

    Create 문자열과 문자 포인터의 개념.c

---
+++ 문자열과 문자 포인터의 개념.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+	char* str = "C language Programing", * s;       // str과 s라는 포인터 변수 선언. str은 C의 주솟값을 나타내고 있음
+	s = str;										// s와 str은 서로 주솟값 공유
+	while (*s)										// *s가 NULL만 아니라면 계속 출력
+		printf("%s\n", s++);						// C만 출력되나? 라는 예상(공백은 NULL이 아님)
+
+	return 0;										
+}
+
+// 결과
+// C language Programing      s++로 인해 주솟값이 이동되므로 한칸씩 밀려 출력됨
+//  language Programing
+// language Programing
+// anguage Programing
+// nguage Programing
+// guage Programing
+// uage Programing
+// age Programing
+// ge Programing
+// e Programing
+//  Programing
+// Programing
+// rograming
+// ograming
+// graming
+// raming
+// aming
+// ming
+// ing
+// ng
+// g                       다음은 NULL문자이므로 출력되지 않음
