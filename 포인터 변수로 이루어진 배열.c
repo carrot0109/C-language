a6ec82e03e878910cc68a087973b0fe4a4070c19
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-06-29 16:10:43 +0900

    포인터 변수로 이루어진 배열.c

--- 포인터 변수로 이루어진 배열.c
+++ 포인터 변수로 이루어진 배열.c
@@ -17,9 +17,9 @@ int main()
 {
 	char* strarr[3] = { "simple","string","array" };    
 	printf("%s\n", strarr[0]);         
-	/*strarr이라는 포인터 배열은 simple이라는 char형 포인터 배열 자료를 저장해놓은 배열이다. 
-	simple 이라고 표시는 되어있지만 포인터 자료이기 때문에 문자열의 첫번째 주소만을 가진다. 
-        따라서 srtarr[0]은 simple의 주소값을 가리키므로 따로 참조연산자가 필요하지 않다는 의미다.*/
+	/*str[5]="HAPPY" 를 정의했을 때 prinf("%s",str)를 통해 HAPPY를 출력한 것과 같이 
+ 	문자열을 출력할 때에는 첫 번째 문자의 주솟값을 알기만 하면 출력이 가능하다. 따라서 strarr[0]이라는 것이 simple 중
+	's'의 주소값을 가리키므로 참조연산자가 필요하지 않다. 따라서 숫자와 다르게 참조연산자 없이 그대로 출력할 수 있는 것이다.*/
 
 	
 	printf("%s\n", strarr[1]);
