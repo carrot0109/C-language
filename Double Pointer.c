47720671e6e5ac6a416584b582e4f1dfcc487af1
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-06-29 17:02:24 +0900

    Update Double Pointer.c

--- Double Pointer.c
+++ Double Pointer.c
@@ -5,7 +5,7 @@ int main()
 	static char* str[] = { "C language","java","Python","C++","HTM" };				
 	char** s, i;
 	s = str;
-	for (i = 0; i < 5; i++,*s++)
+	for (i = 0; i < 5; i++,s++)
 	{
 		printf("%p:%p:%s\n", &*s, *s, *s);
 	}
