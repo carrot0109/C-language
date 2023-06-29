247a98289e1e5f14362656ecec198969c1821af6
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-06-29 17:01:35 +0900

    Create Double Pointer.c

---
+++ Double Pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	static char* str[] = { "C language","java","Python","C++","HTM" };				
+	char** s, i;
+	s = str;
+	for (i = 0; i < 5; i++,*s++)
+	{
+		printf("%p:%p:%s\n", &*s, *s, *s);
+	}
+
+	return 0;										
+}
