40d42c1ed7aa021f739ad709e1258436bbc644d4
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-07-03 17:31:38 +0900

    Update struct(7.3).c

--- struct(7.3).c
+++ struct(7.3).c
@@ -9,11 +9,11 @@ struct point
 
 int main()
 {
-	struct point pos1, pos2;
+	struct point pos1, pos2;		// point는 새로운 자료형이라고 생각해야함. 예를 들어 int와 같이
 
 	double distance;
 
-	fputs("point1 pos: ", stdout);
+	fputs("point1 pos: ", stdout);		// fputs는 출력 함수. stdout(표준출력) 
 	scanf_s("%d %d", &pos1.xpos, &pos1.ypos);
 
 	fputs("point2 pos: ", stdout);
