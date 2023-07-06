82bf8b3183210afa55f6f2febe80ec97c8307bbc
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-07-06 17:35:44 +0900

    Update typedef_struct(7.6).c

--- typedef_struct(7.6).c
+++ typedef_struct(7.6).c
@@ -9,10 +9,10 @@ struct point
 	int ypos;
 };
 
-typedef struct point Point;    // struct를 미리 선언하고 typedef 사용
+typedef struct point Point;    	// struct를 미리 선언하고 typedef 사용
 
-typedef struct person          // typedef와 struct를 동시에 사용
-{
+typedef struct person          	// typedef와 struct를 동시에 사용
+{				// 이 경우에는 Person을 동시에 사용해주었기 때문에 따로 person을 정의해주지 않아도 됨. 즉 typedef struct(person)
 	char name[20];
 	char phonenum[20];
 	int age;
