6c6d6ee339a4e6d2af8f7e1a1209fbd265a07d7b
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-07-06 17:32:56 +0900

    Create typedef_struct(7.6).c

---
+++ typedef_struct(7.6).c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct point
+{
+	int xpos;
+	int ypos;
+};
+
+typedef struct point Point;    // struct를 미리 선언하고 typedef 사용
+
+typedef struct person          // typedef와 struct를 동시에 사용
+{
+	char name[20];
+	char phonenum[20];
+	int age;
+}Person;
+
+int main()
+{
+	Point pos = { 10,20 };
+	Person man = { "이승기","014186844",10 };
+	printf("%d %d\n", pos.xpos, pos.ypos);
+	printf("%s %s %d", man.name, man.phonenum, man.age);
+
+	return 0;
+} 
