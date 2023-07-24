fb649ebca6edc78a53d64e31a2016a243306e494
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-07-24 17:58:41 +0900

    Create (7.24)_arrayal.c

---
+++ (7.24)_arrayal.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node* next;
+};
+
+typedef struct node node;
+node* head = NULL, * tail = NULL, * temp = NULL, * queue = NULL;		// 원래 데이터를 망가뜨리지 않기 위해 변수 queue 정의
+void insert(int);
+void delete();
+void printf_queue();
+void release();
+int search(node*);		// 입력된 수의 크기 비교 및 비교를 토대로 원하는 위치에 삽입
+
+int main()
+{
+	int x, d;
+	do {
+		printf("Queue --- 1.삽입, 2.삭제, 3.출력, 0.끝\nInput: ");
+		scanf("%d", &x);
+		switch (x)
+		{
+		case 1:
+			printf("\n데이터 입력: ");
+			scanf("%d", &d);
+			insert(d);
+			break;
+		case 2:
+			delete();
+			break;
+		case 3:
+			printf_queue();
+			break;
+		case 0:
+			release();
+			return 0;
+		}
+		printf("\n\n");
+	} while (1);
+}
+
+void insert(int x) {
+	temp = malloc(sizeof(struct node));
+	temp->data = x;
+	temp->next = NULL;
+
+	int result = search(temp);				// 새로 받은 수를 담고 있는 주소 포인터를 서치함수에 대입
+
+	printf("   %d    \n", result);
+	if (result == 0) {
+		if (tail != NULL)
+			tail->next = temp;		// tail의 주소값을 통해 저장되어있던 next값을 temp의 주소값으로 설정
+		tail = temp;				// tail의 주소를 새로워진 temp의 주소값으로 변경 (값 자체가 바뀌는 게 아니라 메모리 안의 값은 그대로 남아있고 주소만 바뀌는 것
+		if (head == NULL)
+			head = temp;
+	}
+
+	printf(" temp : %d, head : %d, tail : %d \n", temp->data, head->data, tail->data);
+	printf("\n%d가 삽입되었습니다.\n", x);
+	printf_queue();
+	return;
+}
+
+void delete() {
+	int x;
+	if (head == NULL) {
+		printf("\nQueue is empty!!!\n");
+		return;
+	}
+	temp = head;
+	head = head->next;
+	if (!head)
+		tail = NULL;
+	x = temp->data;
+	free(temp);
+	printf("%d가 삭제되었습니다.\n", x);
+	printf_queue();
+	return;
+}
+
+void printf_queue() {
+	if (head == NULL) {
+		printf("Queue is empty!\n");
+		return;
+	}
+	temp = head;
+	printf("현재 큐의 내용(head...tail): ");
+	while (temp) {
+		printf("%d", temp->data);
+		temp = temp->next;
+	}
+	return;
+}
+
+void release() {
+	while (head) {
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+int search(node* temp) {
+	if (head == NULL)
+		return 0;
+	
+	queue = head;
+	if (queue->data > temp->data && queue == head) {		
+		head = temp;
+		temp->next = queue;
+		return 1;
+	}
+	// 이 조건문을 while문 안에 넣게 되면 값이 처음 하나 입력되었을 때, queue의 next값이 NULL이므로 반복이 종료가 되버려 문제가 발생한다.
+	// 따라서 조건문을 실행하기 전에 입력함으로써 이 문제를 해결한다.
+
+	while (queue->next != NULL) {	// 마지막 queue->next가 tail이고 tail이 NULL이면 반복문 종료
+		
+		if (queue->data < temp->data && queue->next->data > temp->data) {
+			temp->next = queue->next;
+			queue->next = temp;
+			return 1;
+		}
+			
+		queue = queue->next;		// 주소를 head -> V1 -> V2...로 변경해가며 판단
+	}
+	return 0;
+}
